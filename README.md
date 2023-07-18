c2f6106c20c02287005d5643203779a905b9cc05
rafi1919 <rafifourze1904@gmail.com>
2023-07-18 21:30:03 +0700

    updated

---
+++ README.md
@@ -0,0 +1,26 @@
+# vue-mekaverse
+
+https://mekaverse-vue.vercel.app/
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+# Vue_mekaverse
+# Vue_mekaverse
+# Vue_mekaverse
+# Vue_mekaverse
+# vue-mekaverse
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="home">
+    <Header />
+    <Collection />
+    <Choose />
+    <About />
+    <!-- <Roadmap /> -->
+    <Asked />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Collection from '@/components/Collection.vue'
+import Choose from '@/components/Choose.vue'
+import About from '@/components/About.vue'
+import Roadmap from '@/components/Roadmap.vue'
+import Asked from '@/components/Asked.vue'
+import Footer from '@/components/Footer.vue'
+
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    Header,
+    Collection,
+    Choose,
+    About,
+    Roadmap,
+    Asked,
+    Footer
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
